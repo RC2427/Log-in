b507676a9023f2cf3f4b264933edf372a360d86c
Rohit Chaudhari <43547779+RC2427@users.noreply.github.com>
2020-07-05 16:48:50 +0530

    Add files via upload

--- js/password.js
+++ js/password.js
@@ -1,8 +1,16 @@
-$(".reveal").on('click',function() {
-    var $pwd = $(".pwd");
-    if ($pwd.attr('type') === 'password') {
-        $pwd.attr('type', 'text');
-    } else {
-        $pwd.attr('type', 'password');
+$("#password-field").on("click",".eye-button",function(e) {
+    e.preventDefault();
+    var passwordId = $(this).find("i").attr("id");
+    var inputID = (passwordId === "eye-font") ? "#pass" : "#pass2";
+    console.log(passwordId, " : ", inputID)
+    $("#" + passwordId).toggleClass("fa-eye fa-eye-slash");
+    if($(inputID).attr("type") === "password"){
+        $(inputID).attr("type","text");
+    }
+    else{
+        $(inputID).attr("type","password");
     }
 });
+
+
+
